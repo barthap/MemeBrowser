363376a1de57aef248f6a066ff94bb8d2e671db1
Bartłomiej Klocek <barthap10@gmail.com>
2020-08-19 14:32:53 +0200

    Configure project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['jest'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': [
+      'warn',
+      {
+        skipUndeclared: true,
+      },
+    ],
+    'react/display-name': 'off',
+  },
+  env: {
+    'jest/globals': true,
+  },
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# Meme Browser
+
+Image gallery app with search-by-text feature. The purpose for this app is collecting images containing text
+and easily finding them.
+
+Written in React Native 💙 Expo. 
